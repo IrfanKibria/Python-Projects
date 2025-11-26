7bb1bacee17f917c2d29bfeebf34f7df68df5116
IrfanKibria <irfan.py3@gmail.com>
2025-11-26 15:04:51 +0600

    Calculator with function solved

--- 02.Calculator_withFunction.py
+++ 02.Calculator_withFunction.py
@@ -1,7 +1,7 @@
 def operation():
     while True:
-        action = input("Enter what operation you want to do: add/sub/div/mul/exit: ")
-        if action.lower() in ['add', 'sub','mul','div','exit']:
+        action = input("Enter what operation you want to do: add/sub/div/mul/exit: ").lower()
+        if action in ['add', 'sub','mul','div','exit']:
             return action
         else:
             print(f"{action} this is invalid, Please Input valid action")
@@ -9,8 +9,8 @@ def operation():
 def numbers():
     while True:
         try:
-            x = int("Enter a First Number: ")
-            y = int("Enter a second Number: ")
+            x = int(input("Enter a First Number: "))
+            y = int(input("Enter a second Number: "))
             return x,y
         except ValueError as v:
             print("Enter valid integer Number")
@@ -30,6 +30,29 @@ def div(x, y):
     else:
         return x/y
     
-def calculation():
+def calculator():
     while True:
-        
+        ac = operation()
+        if ac == 'exit':
+            print("Thank You for using Calculator")
+            break
+
+        x, y = numbers()
+
+        if ac == 'add':
+            print(f'{x} + {y} = {add(x,y)}')
+
+        if ac == 'sub':
+            print(f'{x} - {y} = {sub(x,y)}')
+
+        if ac == 'mul':
+            print(f'{x} * {y} = {mul(x,y)}')
+
+        if ac == 'div':
+            result = div(x,y)
+            if result is None:
+                print("Any number cant divided by 0")
+            else:
+                print(f"{x} / {y} = {result}")
+
+calculator()
